0a5d8b8d8b795622d0441f291ca97f25a1b7b62e
lamdarv <lamdarvs@gmail.com>
2023-07-10 14:46:43 +0700

    Create Employee CRUD With .NET Core 6.0

---
+++ Views/Employee/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EmployeeCRUD.Views.Employee
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
